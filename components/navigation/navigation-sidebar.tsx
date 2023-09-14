ca7d34216538768d7f6cbabd4e6db3be9f4d6752
VJS <minsung_96@naver.com>
2023-09-14 23:59:56 +0900

    Channel Profile + Mode Toggle

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -2,9 +2,14 @@ import { redirect } from "next/navigation"
 
 import { currentProfile } from "@/lib/current-profile"
 import { db } from "@/lib/db"
-import { NavigationAction } from "./navigation-action"
+import { UserButton } from "@clerk/nextjs"
+
 import { Separator } from "@/components/ui/separator"
 import { ScrollArea } from "@/components/ui/scroll-area"
+import { ModeToggle } from "@/components/mode-toggle"
+
+import { NavigationItem } from "./navigation-item"
+import { NavigationAction } from "./navigation-action"
 
 export const NavigationSidebar = async () => {
     const profile = await currentProfile()
@@ -31,11 +36,26 @@ export const NavigationSidebar = async () => {
             />
             <ScrollArea className="flex-1 w-full">
                 {servers.map((server) => (
-                    <div key={server.id}>
-                        {server.name}
+                    <div key={server.id} className="mb-4">
+                        <NavigationItem 
+                            id={server.id}
+                            name={server.name}
+                            imageUrl={server.imageUrl}
+                        />
                     </div>
                 ))}
             </ScrollArea>
+            <div className="pb-3 mt-auto flex items-center flex-col gap-y-4">
+                <ModeToggle />
+                <UserButton 
+                    afterSignOutUrl="/"
+                    appearance={{
+                        elements: {
+                            avatarBox: "h-[48px] w-[48px]"
+                        }
+                    }}
+                />
+            </div>
         </div>
     )
 }
