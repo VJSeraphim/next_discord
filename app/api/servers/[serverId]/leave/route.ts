897607f30cdf662351d723ba61c8a999ce7d54ff
VJS <minsung_96@naver.com>
2023-09-25 17:51:03 +0900

    Leave + Delete functionality + Server, Members Search

--- app/api/servers/[serverId]/leave/route.ts
+++ app/api/servers/[serverId]/leave/route.ts
@@ -1,12 +1,10 @@
 import { currentProfile } from "@/lib/current-profile"
-import { NextResponse } from "next/server"
-import { v4 as uuidv4 } from "uuid"
-
 import { db } from "@/lib/db"
+import { NextResponse } from "next/server"
 
 export async function PATCH(
     req: Request,
-    { params } : { params: { serverId: string } } 
+    { params } : { params: { serverId: string } }
 ) {
     try {
         const profile = await currentProfile()
@@ -15,23 +13,33 @@ export async function PATCH(
             return new NextResponse("Unauthorized", { status: 401 })
         }
         if(!params.serverId) {
-            return new NextResponse("Invalid Server Id", { status: 400 })
+            return new NextResponse("Server ID Missing", { status: 400})
         }
 
         const server = await db.server.update({
             where: {
                 id: params.serverId,
-                profileId: profile.id
+                profileId:{
+                    not: profile.id
+                },
+                members: {
+                    some: {
+                        profileId: profile.id
+                    }
+                }
             },
             data: {
-                inviteCode: uuidv4()
+                members: {
+                    deleteMany: {
+                        profileId: profile.id
+                    }
+                }
             }
         })
 
         return NextResponse.json(server)
-        
     } catch (error) {
-        console.log("[SERVER_ID]", error)
+        console.log("[SERVER_ID_LEAVE]", error)
         return new NextResponse("Internal Server Error", { status: 500 })
     }
 }
--- components/modals/delete-server-modal.tsx
+++ components/modals/delete-server-modal.tsx
@@ -1,49 +1,40 @@
 "use client"
+import axios from 'axios'
 import { useState } from 'react'
-import { Check, Copy, RefreshCw } from 'lucide-react'
+import { useRouter } from 'next/navigation'
 
 import {
     Dialog,
     DialogContent,
     DialogHeader,
-    DialogTitle
+    DialogTitle,
+    DialogDescription,
+    DialogFooter
 } from '@/components/ui/dialog'
-import { Label } from '@/components/ui/label'
-import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 
 import { useModal } from "@/hooks/use-modal-store"
-import { useOrigin } from '@/hooks/use-origin'
-import axios from 'axios'
+
 
 
 export const DeleteServerModal = () => {
-    const { onOpen, isOpen, onClose, type, data } = useModal()
-    const origin = useOrigin()
+    const { isOpen, onClose, type, data } = useModal()
+    const router = useRouter()
 
-    const isModalOpen = isOpen && type === "invite"
+    const isModalOpen = isOpen && type === "deleteServer"
     const { server } = data
 
-    const [copied, setCopied] = useState(false)
     const [isLoading, setIsLoading] = useState(false)
 
-    const inviteUrl = `${origin}/invite/${server?.inviteCode}`
-
-    const onCopy = () => {
-        navigator.clipboard.writeText(inviteUrl)
-        setCopied(true)
-
-        setTimeout(() => {
-            setCopied(false)
-        }, 1000)
-    }
-
-    const onNew = async() => {
+    const onClick = async () => {
         try {
             setIsLoading(true)
-            const response = await axios.patch(`api/servers/${server?.id}/invite-code`)
-        
-            onOpen("invite", { server: response.data })
+
+            await axios.delete(`/api/servers/${server?.id}`)
+            
+            onClose()
+            router.refresh()
+            router.push("/")
         } catch (error) {
             console.log(error)
         } finally {
@@ -56,36 +47,31 @@ export const DeleteServerModal = () => {
             <DialogContent className="bg-white text-black p-0 overflow-hidden">
                 <DialogHeader className="pt-8 px-6">
                     <DialogTitle className="text-2xl text-center font-bold">
-                        Invite Friends
+                        Delete Server
                     </DialogTitle>
+                    <DialogDescription className="text-center text-zinc-500">
+                        Are you sure you want to Delete <span className="font-semibold text-indigo-500">{server?.name}</span> server? <br/>
+                        Your server will be <span className="font-bold text-rose-500">PERMANANTLY DELETED!</span>
+                    </DialogDescription>
                 </DialogHeader>
-                <div className="p-6">
-                    <Label
-                        className='uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70'
-                    >
-                        Server Invitation Link
-                    </Label>
-                    <div className="flex items-center mt-2 gap-x-2">
-                        <Input 
+                <DialogFooter className="bg-gray-100 px-6 py-4">
+                    <div className="flex items-center justify-between w-full">
+                        <Button
                             disabled={isLoading}
-                            className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
-                            value={inviteUrl}
-                        />
-                        <Button disabled={isLoading} onClick={onCopy} size="icon">
-                            {copied ? <Check className="w-4 h-4"/> :  <Copy className="w-4 h-4"/>}
+                            onClick={onClose}
+                            variant="ghost"
+                        >
+                            Cancel
+                        </Button>
+                        <Button
+                            disabled={isLoading}
+                            onClick={onClick}
+                            variant="alert"
+                        >
+                            Delete
                         </Button>
                     </div>
-                    <Button
-                        onClick={onNew}
-                        disabled={isLoading}
-                        variant="link"
-                        size="sm"
-                        className="text-xs text-zinc-500 mt-4"
-                    >
-                        Generate new link
-                        <RefreshCw className="w-4 h-4 ml-2"/>
-                    </Button>
-                </div>
+                </DialogFooter>
             </DialogContent>
         </Dialog>
     )
